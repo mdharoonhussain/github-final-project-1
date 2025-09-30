4146fef293901049bba5b375e6341d1da114a231
MD HAROON HUSSAIN <115461320+mdharoonhussain@users.noreply.github.com>
2025-09-30 18:24:41 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,4 @@
 
    echo "Enter the principal:"
    read p
-   echo "Enter rate of interest per year:"
-   read r
-   echo "Enter time period in years:"
-   read t
-   s=`expr $p \* $t \* $r / 100`
-   echo "The simple interest is: "
-   echo $s
+
